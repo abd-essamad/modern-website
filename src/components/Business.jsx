2117eff2a66a7e4dfe90b8219ad0f2f1c3e9353d
elmaaroufi1 <abdessamad.maaroufi12@gmail.com>
2022-10-01 19:13:03 +0100

    ready to deploy

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,6 +3,26 @@ import style, {layout} from '../style'
 import  Button from './Button'
 import FeedbackCard from './FeedbackCard'
 
+const FeatureCard = ({icon, title, content, index})=>(
+  <div className={`flex flex-row p-6 rounded-[20px]
+  ${index !== features.length - 1 ? 'mb-6': 'mb-0'} feature-card`} >
+      <div className={`w-[64px] h-[64px] 
+      rounded-full ${style.flexCenter} bg-dimBlue `} >
+        <img src={icon} alt="icon"
+        className=' w-[50%] h-[50%] object-contain '  />
+      </div>
+      <div className='flex-1 flex flex-col ml-3' >
+        <h4 className='font-poppins font-semiblod
+        text-white text-[18px] leading-[23px] mb-1 ' >
+         {title}
+        </h4>
+        <p className='font-poppins font-semiblod
+        text-dimWhite text-[16px] leading-[24px] mb-1 ' >
+          {content}
+        </p>
+      </div>
+  </div>
+)
 const Business = () =>(
     <section id='features' className={layout.section} >
       <div className={layout.sectionInfo}>
@@ -20,7 +40,8 @@ const Business = () =>(
       <div className={`${layout.sectionImg} flex-col
        `} >
          {features.map((feature,index) =>(
-           FeedbackCard
+           <FeatureCard key={feature.id}
+           {...feature} index={index} />
          ))}
       </div>
     </section>
